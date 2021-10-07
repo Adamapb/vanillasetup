47f8d088b86cba17723f8f1ce9e095c33f6d8a64
Adamapb <89477210+Adamapb@users.noreply.github.com>
2021-10-07 16:27:27 -0400

    Update VanillaSetup.sh

--- VanillaSetup.sh
+++ VanillaSetup.sh
@@ -2,27 +2,25 @@
 sudo -1
 #Enter password
 apt-get update
-apt-get upgrade
+apt-get upgrade -y
 apt install apache2 -y
 apt install mysql-server -y
 mysql_secure_installation
-0
-n
-n
-n
-y
+echo n
+echo 12345
+echo y
+echo n
+echo n
+echo y
 systemctl enable apache2
 systemctl enable mysql
-add-apt-repository ppa:ondrej/php
+add-apt-repository ppa:ondrej/php -y
 apt-get update
-apt install php7.3 libapache2-mod-php7.3 php7.3-common php7.3-mysql php7.3-cli php7.3-opcache php7.3-gd php7.3-curl php7.3-cli php7.3-imap php7.3-mbstring php7.3-soap php7.3-xmlrpc php7.3-xml php7.3-zip
-wget https://open.vanillaforums.com/get/vanilla-core-3-1.zip
-unzip vanilla-core-3-1.zip -d /var/www
-rm vanilla-core-3-1.zip
-mv /var/www/package /var/www/vanilla
-sudo chown -R www-data:www-data /var/www/vanilla
-apt install mysqladmin -y
-
+apt install php7.3 libapache2-mod-php php7.3-common php7.3-mysql php7.3-cli php7.3-opcache php7.3-gd php7.3-curl php7.3-imap php7.3-mbstring php7.3-soap php7.3-xmlrpc php7.3-xml php7.3-zip -y
+cd /var/www/
+git clone https://github.com/Adamapb/vanillasetup.git
+mv /var/www/vanillasetup /var/www/vanilla
+chown -R www-data:www-data /var/www/vanilla
 mysql -u root -e "CREATE DATABASE vanilla;"
 mysql -u root -e "CREATE USER src1@localhost IDENTIFIED BY 'Password12345!';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON vanilla.* TO src1@localhost;"
@@ -47,6 +45,6 @@ cat > /etc/apache2/sites-available/mydomain.conf <<"__EOF__"
 __EOF__
 
 a2ensite mydomain.conf
-sudo systemctl reload apache2
+systemctl reload apache2
 
 
